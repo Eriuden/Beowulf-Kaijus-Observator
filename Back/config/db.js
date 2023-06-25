e3a58a03f02797d8b12541fc269cd3fdecfe9b0c
Antoine Carrette <71089756+Eriuden@users.noreply.github.com>
2023-06-25 15:36:47 +0200

    Update db.js

--- Back/config/db.js
+++ Back/config/db.js
@@ -4,7 +4,7 @@ mongoose
   .connect(
     "mongodb+srv://" +
       process.env.DB_USER_PASS +
-      "@cluster0.iodcc.mongodb.net/KaijuObservator"
+      
   )
   .then(() => console.log("connecté à mongoDB"))
   .catch((err) => console.log("echec de connection à mongoDB", err));

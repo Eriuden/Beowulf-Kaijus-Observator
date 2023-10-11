3475d5902eba0f683daa3ec3ff3f050ce4adefb8
Eriuden <antoinecarrette@hotmail.fr>
2023-10-11 20:46:07 +0200

    context fixes

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,30 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import useDispatch from "react-redux"
 
 function App() {
+  const {uid, setUid} = useState(null)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchToken =async () => {
+    await axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}jwtid`,
+      withCredentials: true
+    })
+    .then((res) => {
+      console.log(res);
+      setUid(res.data)
+    })
+    .catch(() => console.log("Pas de tokens"))
+    }
+    fetchToken()
+
+    if (uid) dispatch(getUser(uid))
+  }, [uid])
+  
   return (
     <div className="App">
       <Routes>
